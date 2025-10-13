ed6c0c58fc7fa955287d28a5b5350ab0b089e0bd
daadaamed <daadaamed@gmail.com>
2025-10-13 11:51:17 +0200

    Feature API : Add room column in entity

--- glados-api-draft-master/app/glados/api/entity/serializers.py
+++ glados-api-draft-master/app/glados/api/entity/serializers.py
@@ -12,6 +12,7 @@ class EntitiesRequestSerializer(ma.Schema):
 
 class EntitySerializer(ma.Schema):
     created_at = fields.DateTime("%Y-%m-%dT%H:%M:%S")
+    room = fields.Function(lambda obj: obj.room.name if getattr(obj, "room", None) else None)
 
     class Meta:
         model = Entity
@@ -22,7 +23,8 @@ class EntitySerializer(ma.Schema):
             "type",
             "status",
             "value",
-            "created_at"
+            "created_at",
+            "room"
         ]
 
 
